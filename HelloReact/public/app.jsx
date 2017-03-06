b5b7e428343b8e97515339a15605c7cb5d756409
pinoytech <forum.providence@yahoo.com>
2017-03-07 02:48:53 +0800

    Setting state

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,17 +1,32 @@
 var Greeter = React.createClass({
+
   getDefaultProps: function () {
     return {
       name: 'React',
       message: 'This is the default message'
     }
   },
+  getInitialState: function() {
+    return {
+      name: this.props.name
+    };
+  },
+  onButtonClick: function(event) {
+    event.preventDefault();
+    var name = this.refs.name.value;
+    this.setState({name: name});
+  },
   render: function() {
-    const name = this.props.name;
+    const name = this.state.name;
     const message = this.props.message;
     return (
       <div>
-        <h1 className="test">Hello {name}</h1>
+        <h1 className="test">Hello {this.state.name}</h1>
         <p>{message}</p>
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name" />
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
