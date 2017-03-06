42543188f7c31363dd94a8ef12965f6043d3228d
pinoytech <forum.providence@yahoo.com>
2017-03-07 02:08:47 +0800

    Components

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,14 @@
+var Greeter = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Hello React</h1>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello React</h1>,
+  <Greeter />,
   document.getElementById('app')
 );
