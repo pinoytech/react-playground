0e451d5476cd9ab787aaae7bcdfc1a2c308f1d8c
pinoytech <forum.providence@yahoo.com>
2017-03-07 02:29:14 +0800

    adding additional props

--- public/app.jsx
+++ public/app.jsx
@@ -1,21 +1,25 @@
 var Greeter = React.createClass({
   getDefaultProps: function () {
     return {
-      name: 'React'
+      name: 'React',
+      message: 'This is the default message'
     }
   },
   render: function() {
     const name = this.props.name;
+    const message = this.props.message;
     return (
       <div>
         <h1 className="test">Hello {name}</h1>
-        <p>This is from a component</p>
+        <p>{message}</p>
       </div>
     );
   }
 });
 
+var firstName = 'Teejay';
+
 ReactDOM.render(
-  <Greeter />,
+  <Greeter name={firstName} message="Message from prop!"/>,
   document.getElementById('app')
 );
