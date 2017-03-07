6fc75e47b49dbbd944bf6ca734b350816e0b8172
pinoytech <forum.providence@yahoo.com>
2017-03-07 12:28:58 +0800

    adding a webpack.config.js for webpack

---
+++ HelloReactWebPack/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: './public/app.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    extensions: [
+      '', '.js', 'jsx'
+    ]
+  }
+}
