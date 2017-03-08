dbabe07fffd23fb86a94e85fa708d197e5c0a4ac
pinoytech <forum.providence@yahoo.com>
2017-03-08 11:41:31 +0800

    Using componentWillMount to get data using our action creator

--- blog/src/components/posts_index.js
+++ blog/src/components/posts_index.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import fetchPosts from '../actions/index';
 
-export default () => {
-  return <div>List of blog posts</div>;
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+  render() {
+    return (
+      <div>
+        List of blog posts
+      </div>
+    )
+  }
 }
+
+function mapStateToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch);
+}
+export default connect(null, mapStateToProps)(PostsIndex);
