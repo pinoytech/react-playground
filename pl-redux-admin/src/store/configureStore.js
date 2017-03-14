b5e94d16f85aab7c2a096a8a08141007f9ecd45d
pinoytech <forum.providence@yahoo.com>
2017-03-15 07:53:51 +0800

    removed commented out code from pl-redux-admin

--- pl-redux-admin/src/store/configureStore.js
+++ pl-redux-admin/src/store/configureStore.js
@@ -1,12 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
-// import reduxImmutableStateVariant from 'redux-immutable-state-invariant';
 
 function configureStore(initalState) {
   return createStore(
     rootReducer,
     initalState
-    // applyMiddleware(reduxImmutableStateVariant())
   );
 }
 
