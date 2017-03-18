500ceb9a1e844ec51413965c87d564bbcc21c7a6
pinoytech <forum.providence@yahoo.com>
2017-03-18 08:37:40 +0800

    showing a post and deleting a post

--- step-redux-blog/src/components/posts_index.js
+++ step-redux-blog/src/components/posts_index.js
@@ -8,19 +8,41 @@ class PostsIndex extends Component {
   componentWillMount() {
     this.props.fetchPosts();
   }
+
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return <li className="list-group-item" key={post.id}>
+        <Link to={"posts/" + post.id}>
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+        </Link>
+      </li>
+    });
+  }
+
   render() {
     return (
       <div>
         <div className="text-xs-right">
           <Link to="/posts/new" className="btn btn-primary">Add A Post</Link>
         </div>
-        List of blog posts
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     )
   }
 }
 
-function mapStateToProps(dispatch) {
+function mapDispatchToProps(dispatch) {
   return bindActionCreators({ fetchPosts }, dispatch);
 }
-export default connect(null, mapStateToProps)(PostsIndex);
+
+function mapStateToProps(state) {
+  return {
+    posts: state.posts.all
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex);
